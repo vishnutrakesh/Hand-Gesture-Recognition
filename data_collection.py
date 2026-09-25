cd6d2ece9edbc8903a704c66f52f95078a429634
Vishnu T Rakesh <vishnutrakesh@gmail.com>
2026-09-25 23:46:21 +0530

    made some changes

--- data_collection.py
+++ data_collection.py
@@ -1,33 +1,29 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
-from cvzone.HandTrackingModule import HandDetector
+import time
 
-cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=1)
- 
-offset = 50
-imgSize = 400
- 
+cap=cv2.VideoCapture(0)
+detector=HandDetector(maxHands=1)
+offset=20
+imgSize=300
+counter=0
+
+folder="C:/Users/Vishnu/Documents/College/5th Sem/Projects/Hand Gesture Recognition/data/A"
+
 while True:
-    success, img = cap.read()
-    hands, img = detector.findHands(img)
-
+    success,img=cap.read()
+    hands,img=detector.findHands(img)
     if hands:
-        hand = hands[0]
+        hand=hands[0]
         x,y,w,h=hand['bbox']
 
         imgWhite=np.ones((imgSize,imgSize,3),np.uint8)*255
-        x1=max(0,x-offset)
-        y1=max(0,y-offset)
-        x2=min(x+w+offset,img.shape[1])
-        y2=min(y+h+offset,img.shape[0])
-        imgCrop=img[y1:y2,x1:x2]
-
+        imgCrop=img[y-offset:y+h+offset,x-offset:x+w+offset]
         imgCropShape=imgCrop.shape
-        imgWhite[0:imgCrop.shape[0],0:imgCrop.shape[1]]=imgCrop
 
-        aspectRatio = h/w
+        aspectRatio=h/w
 
         if aspectRatio>1:
             k=imgSize/h
@@ -36,7 +32,6 @@ while True:
             imgResizeShape=imgResize.shape
             wGap=math.ceil((imgSize-wCal)/2)
             imgWhite[:,wGap:wCal+wGap]=imgResize
-
         else:
             k=imgSize/w
             hCal=math.ceil(k*h)
@@ -45,9 +40,12 @@ while True:
             hGap=math.ceil((imgSize-hCal)/2)
             imgWhite[hGap:hCal+hGap,:]=imgResize
 
-        if imgCrop.size>0:
-            cv2.imshow("Image Crop", imgCrop)
-            cv2.imshow("Image White", imgWhite)
+        cv2.imshow("ImageCrop",imgCrop)
+        cv2.imshow("ImageWhite",imgWhite)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    cv2.imshow("Image",img)
+    key=cv2.waitKey(1)
+    if key==ord("a"):
+        counter+=1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        print(counter)
